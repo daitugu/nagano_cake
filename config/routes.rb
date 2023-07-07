174a39b5ce70f6c8311be8e51f8ec29743d70664
daitugu <132569322+daitugu@users.noreply.github.com>
2023-07-07 06:08:29 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
+
+
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
  scope module: :public do
    root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
  resources :items
- 
+
 
  end
   namespace :admin do
-      
+
   resources :items
   end
 end
