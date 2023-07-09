12662e38dd35f60c61d5a5c516e0ab0169a6a106
daitugu <132569322+daitugu@users.noreply.github.com>
2023-07-09 10:05:37 +0000

    九日

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
-  def configure_permitted_parameters
+  def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana ,:postal_code ,:address ,:telephone_number])
   end
   # GET /resource/sign_up
