08b01194cf1851692af2ec6a8db694457368edc5
daitugu <132569322+daitugu@users.noreply.github.com>
2023-07-11 06:23:05 +0000

    十一

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,14 +3,15 @@ def new
   @item = Item.new
 end
 def index
-  @items = Item.all
+  @items = Item.all.page(params[:page])
 end
 def create
-    @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_root_path
+  @item = Item.new(item_params)
+  @item.save
+  redirect_to admin_root_path
 end
 def show
+    @item = Item.find(params[:id])
 end
 def edit
 end
